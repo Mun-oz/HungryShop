4380f3ae5b98953ea95919fddb8bc34b1a15898d
Mun-oz <mar.munozm@duocuc.cl>
2026-04-16 12:29:24 -0400

    Se añadio @Table a las clases Producto, Cliente y Pedido

--- src/main/java/com/duoc/hungryshop/model/Cliente.java
+++ src/main/java/com/duoc/hungryshop/model/Cliente.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -13,6 +14,7 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Table(name = "clientes")
 public class Cliente {
 
     @Id
--- src/main/java/com/duoc/hungryshop/model/Producto.java
+++ src/main/java/com/duoc/hungryshop/model/Producto.java
@@ -3,6 +3,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -10,10 +11,12 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+
 @Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Table(name = "productos")
 public class Producto {
 
     @Id
